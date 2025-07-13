eb3f986627e7677e52f3a552893df419009ffcc2
Phạm Thế An <anptse184524@fpt.edu.vn>
2025-07-13 21:51:59 +0700

    test ok

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    // globals: true, // Temporarily commented out
     environment: 'jsdom',
-    // setupFiles: './src/test/setup.ts', // Temporarily commented out
+    globals: true,
+    setupFiles: './src/test/setup.ts',
   },
-});
+} as any);
